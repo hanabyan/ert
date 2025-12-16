3eae48e26e563f58074782c0f20ba5fdbc8f56b5
budi <bud1.setia1@gmail.com>
2025-12-16 09:03:12 +0700

    adjust login layout

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Layout, Card, Form, Input, Button, Alert, Typography } from 'antd';
+import { Layout, Card, Form, Input, Button, Alert, Typography, Flex } from 'antd';
 import { LoginOutlined } from '@ant-design/icons';
 import { useAuth } from '../context/AuthContext';
 
@@ -35,38 +35,40 @@ export default function LoginPage() {
     };
 
     return (
-        <Content style={{ padding: '50px', maxWidth: 400, margin: '0 auto' }}>
-            <Card>
-                <Title level={2} style={{ textAlign: 'center' }}>
-                    <LoginOutlined /> Login
-                </Title>
-                
-                <Form form={form} onFinish={handleSubmit} layout="vertical">
-                    <Form.Item
-                        label="Username"
-                        name="username"
-                        rules={[{ required: true, message: 'Masukkan username!' }]}
-                    >
-                        <Input size="large" />
-                    </Form.Item>
+        <Flex justify="center" align="center" style={{ minHeight: '100vh' }}>
+            <Content className="login-page" style={{ padding: '50px', maxWidth: 400, margin: '0 auto' }}>
+                <Card>
+                    <Title level={2} style={{ textAlign: 'center' }}>
+                        <LoginOutlined /> Login
+                    </Title>
+                    
+                    <Form form={form} onFinish={handleSubmit} layout="vertical">
+                        <Form.Item
+                            label="Username"
+                            name="username"
+                            rules={[{ required: true, message: 'Masukkan username!' }]}
+                        >
+                            <Input size="large" />
+                        </Form.Item>
 
-                    <Form.Item
-                        label="Password"
-                        name="password"
-                        rules={[{ required: true, message: 'Masukkan password!' }]}
-                    >
-                        <Input.Password size="large" />
-                    </Form.Item>
+                        <Form.Item
+                            label="Password"
+                            name="password"
+                            rules={[{ required: true, message: 'Masukkan password!' }]}
+                        >
+                            <Input.Password size="large" />
+                        </Form.Item>
 
-                    {error && <Alert title={error} type="error" showIcon style={{ marginBottom: 16 }} />}
+                        {error && <Alert title={error} type="error" showIcon style={{ marginBottom: 16 }} />}
 
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" loading={loading} size="large" block>
-                            Login
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Card>
-        </Content>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" loading={loading} size="large" block>
+                                Login
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Card>
+            </Content>
+        </Flex>
     );
 }
